7993cd282bad10175077fc89259b83100cee378a
David Edson <eddo888@tpg.com.au>
2020-04-14 22:17:08 +1000

    explicit paths

--- setup.py
+++ setup.py
@@ -6,34 +6,36 @@ from setuptools import setup
 
 pwd = path.abspath(path.dirname(__file__))
 with codecs.open(path.join(pwd, 'README.md'), 'r', encoding='utf8') as input:
-    long_description = input.read()
+	long_description = input.read()
 
-version='1.1'
-	
+version = '1.2'
+
 setup(
 	name='McUnix',
 	version=version,
 	license='MIT',
-    long_description=long_description,
+	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url='https://github.com/eddo888/McUnix',
-	download_url='https://github.com/eddo888/McUnix/archive/%s.tar.gz'%version,
+	download_url='https://github.com/eddo888/McUnix/archive/%s.tar.gz' % version,
 	author='David Edson',
 	author_email='eddo888@tpg.com.au',
-	packages=[
-		'McUnix'
-	],
+	packages=['McUnix'],
 	install_requires=[
 		'argcomplete',
-		'libxml2-python3',
 		'xmltodict',
 		'pytz',
 		'Baubles',
 		'Perdy',
 		'Argumental',
 	],
-	scripts=list(map(
-		lambda x: 'bin/%s'%x, 
-		os.listdir('bin')
-	)),
+	scripts=[
+		"bin/mail.py", 
+		"bin/tail.py", 
+		"bin/url.py", 
+		"bin/touch.py",
+		"bin/diff.py", 
+		"bin/path.py", 
+	], 
 )
+
